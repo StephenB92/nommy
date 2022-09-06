882c4325c56ccde6f4ac2f07705fabdc8efb9010
StephenB92 <stephennb92@gmail.com>
2022-09-06 19:47:22 +0000

    Create admin and add recipe and comment features to admin site

--- meal_planner/admin.py
+++ meal_planner/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Recipe, Comment
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('dish_name',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('status', 'created_on', 'slug', 'dish_name')
+    search_fields = ('dish_name', 'description')
+    summernote_fields = ('description', 'ingredients', 'method')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'body', 'dish_name', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
